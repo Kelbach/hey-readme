e01a910481a900863da395da3645253772aabb0b
Kelbach <87092340+Kelbach@users.noreply.github.com>
2021-10-18 00:51:35 -0400

    need to loop collaborators

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# test
 
+  # TEST
+
+  ## Description
+  no thanks
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  ## Installation
+  windows download mananger
+  
+  ## Usage
+  upside down
+
+  undefined
+
+  
+  
+  ## Guidelines for Contribution
+    please be kind to each other
+    
+
+  ## Tests
+  i put the thing in node and bippity boppity coding magic
+
+  ##Questions, Comments, Concerns
+  GitHub Username: k
+  Email Address: email
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = input => {
+  if (input === 'No License') {
+    return ''
+  } else {
+    return `[![Badge](https://img.shields.io/badge/License-${input.replace(/ /g, '_')}-yellow)](#license)`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseSection = input => {
+  if (input === 'No License') {
+    return ''
+  } else {
+    return `## License
+    This project is covered under the ${input} license.
+    `
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderCredits = data => {
+  if (data.confirmCredit === false) {
+    return ''
+  } else {
+    //something about rendering names and links
+  }
+}
+
+const renderContribution = data => {
+  if (data.confirmContribution === false) {
+    return `## Guidelines for Contribution
+    This project follows the guidelines of the [Contributor's Covenant](https://www.contributor-covenant.org/).
+    `
+  } else {
+    return `## Guidelines for Contribution
+    ${data.contribution}
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = data => {
+  return `
+  # ${data.title}${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ${renderCredits(data)}
+
+  ${renderLicenseSection(data.license)}
+  
+  ${renderContribution(data)}
+
+  ## Tests
+  ${data.test}
+
+  ##Questions, Comments, Concerns
+  GitHub Username: ${data.github}
+  Email Address: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
 
-
 //title with license badge/link
-
 //description
-
 //table of contents
-
 //installation
-
 //usage
-
 //license
-
 //contributing guidelines
-
 //tests
-
 //questions
